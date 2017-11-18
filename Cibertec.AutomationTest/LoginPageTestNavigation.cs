d7363bedf1608a0db3465e6354bcd74ffe566171
Alexander Rodriguez <alexrod2121@gmail.com>
2017-11-18 12:31:55 -0500

    Agregar archivos de proyecto.

---
+++ Cibertec.AutomationTest/LoginPageTestNavigation.cs
@@ -0,0 +1,43 @@
+﻿using Cibertec.Automation;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cibertec.AutomationTest
+{
+    public class LoginPageTestNavigation
+    {
+        public LoginPageTestNavigation()
+        {
+            Driver.GetInstance(DriversOption.Chrome);
+        }
+
+        [Fact]
+        public void LoginTest()
+        {
+            LoginPage.Go();
+            LoginPage.LoginAs("alexrod2121@gmail.com").WithPassword("password").Login();
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            LoginPage.GetUrl().Should().Be("http://localhost/angular/#!/home");
+            LoginPage.Logout();
+            Driver.CloseInstance();
+        }
+
+        [Fact]
+        public void LoginWrongTest()
+        {
+            LoginPage.Go();
+            LoginPage.LoginAs("alexrod2121@gmail.com").WithPassword("44879878").Login();
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            LoginPage.IsAlertErrorPresent().Should().BeTrue();
+            Driver.CloseInstance();
+        }
+    }
+}
---
+++ Cibertec.AutomationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cibertec.AutomationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cibertec.AutomationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ea49e72-bc4b-409e-bded-de33f8993657")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
